d9ea28c50f39f8392907c06836269e8ec145c45d
KuperDude <fieldsdev06@gmail.com>
2024-06-11 00:38:48 +0300

    [Clean] add shadow on HomeView text, update text

--- SwiftGitTest/HomeView.swift
+++ SwiftGitTest/HomeView.swift
@@ -9,10 +9,11 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("LOL")
             .padding()
             .background(.orange)
             .cornerRadius(13)
+            .shadow(radius: 10)
     }
 }
 
