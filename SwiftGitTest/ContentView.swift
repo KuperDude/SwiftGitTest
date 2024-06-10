8b78a8cd7cdad89f2d802c855ed2a7a4063cf591
KuperDude <fieldsdev06@gmail.com>
2024-06-10 21:56:25 +0300

    [Feature] add ClickMe button

--- SwiftGitTest/ContentView.swift
+++ SwiftGitTest/ContentView.swift
@@ -14,6 +14,10 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            
+            Button("Click me!") {
+                
+            }
         }
         .padding()
     }
