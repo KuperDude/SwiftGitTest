7f922a7274095a3e5ce6626d5f112ec099cf0d3d
KuperDude <fieldsdev06@gmail.com>
2024-06-11 00:29:38 +0300

    [Feature] create HomeView

---
+++ SwiftGitTest/HomeView.swift
@@ -0,0 +1,21 @@
+//
+//  HomeView.swift
+//  SwiftGitTest
+//
+//  Created by MyBook on 11.06.2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .padding()
+            .background(.orange)
+            .cornerRadius(13)
+    }
+}
+
+#Preview {
+    HomeView()
+}
