c6eb0ba9ec5b7321adb054ed6c5e8c1149e304b1
KuperDude <fieldsdev06@gmail.com>
2024-06-10 18:22:22 +0300

    Initial Commit

---
+++ SwiftGitTest/SwiftGitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftGitTestApp.swift
+//  SwiftGitTest
+//
+//  Created by MyBook on 10.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftGitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
