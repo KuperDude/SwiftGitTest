1871803979daeafef6aafec13349c981de7480b4
KuperDude <fieldsdev06@gmail.com>
2024-06-11 15:27:07 +0300

    [Feature] rename text in HomeView

--- SwiftGitTest/HomeView.swift
+++ SwiftGitTest/HomeView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct HomeView: View {
     var body: some View {
         VStack {
-            Text("LOL")
+            Text("LOL!!!")
                 .padding()
                 .background(.orange)
                 .cornerRadius(13)
