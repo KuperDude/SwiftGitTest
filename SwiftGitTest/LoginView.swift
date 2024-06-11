e62a05620e70a46b455e8ccb940899a8f52cbde0
KuperDude <fieldsdev06@gmail.com>
2024-06-11 15:25:18 +0300

    [Feature] Add LoginView

---
+++ SwiftGitTest/LoginView.swift
@@ -0,0 +1,22 @@
+//
+//  LoginView.swift
+//  SwiftGitTest
+//
+//  Created by MyBook on 11.06.2024.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text("Login here!")
+        
+        Button("Login") {
+            
+        }
+    }
+}
+
+#Preview {
+    LoginView()
+}
