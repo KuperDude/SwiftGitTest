7c2f22ef9c0ebd7072ef5be64fbc20f5276e11bb
KuperDude <fieldsdev06@gmail.com>
2024-06-11 15:29:34 +0300

    [Feature] add SomeView

---
+++ SwiftGitTest/SomeView.swift
@@ -0,0 +1,18 @@
+//
+//  SomeView.swift
+//  SwiftGitTest
+//
+//  Created by MyBook on 11.06.2024.
+//
+
+import SwiftUI
+
+struct SomeView: View {
+    var body: some View {
+        Text("Some text!")
+    }
+}
+
+#Preview {
+    SomeView()
+}
