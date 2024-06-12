ac751a2483871d7a04e1b0379fc1b43951bd0c3b
KuperDude <118105682+KuperDude@users.noreply.github.com>
2024-06-12 23:33:46 +0300

    Create SettingsView (#1)
    [Feature] start working on SettingsView

---
+++ SwiftGitTest/SettingsView.swift
@@ -0,0 +1,31 @@
+//
+//  SettingsView.swift
+//  SwiftGitTest
+//
+//  Created by MyBook on 12.06.2024.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        ZStack {
+            Color.purple
+                .ignoresSafeArea()
+            
+            VStack {
+                
+                Text("Change language")
+                    .padding()
+                    .background(.green)
+                    .foregroundStyle(.white)
+                    .cornerRadius(10)
+            }
+        }
+        
+    }
+}
+
+#Preview {
+    SettingsView()
+}
